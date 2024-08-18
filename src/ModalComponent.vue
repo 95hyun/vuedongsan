1507bb7bd09ffa120326ebd1137f6469e6d747d6
hyun minyoung <kingmandoo95@gmail.com>
2024-08-19 01:39:21 +0900

    feat : watcher - validation (문자열 방지 숫자만 쓸 수 있도록)

--- src/ModalComponent.vue
+++ src/ModalComponent.vue
@@ -5,24 +5,8 @@
       <h4>{{ 원룸들[누른거].title }}</h4>
       <p>{{ 원룸들[누른거].content }}</p>
 <!--      <input @input="month = $event.target.value">-->
-<!--      <input v-model="month">-->
-      <select v-model="month">
-        <option value="1">1개월</option>
-        <option value="2">2개월</option>
-        <option value="3">3개월</option>
-        <option value="4">4개월</option>
-        <option value="5">5개월</option>
-        <option value="6">6개월</option>
-        <option value="7">7개월</option>
-        <option value="8">8개월</option>
-        <option value="9">9개월</option>
-        <option value="10">10개월</option>
-        <option value="11">11개월</option>
-        <option value="12">12개월</option>
-      </select>
-<!--      사용자가 <input> 에 입력한 것은 전부 문자 자료형!-->
-<!--      곱셈은 잘 될 수 있으니, 덧셈하는 순간 문자열 덧셈이 되어버림-->
-<!--      <input v-model.number="month"/> 이렇게 해야 number 타입으로 지정-->
+      <input v-model="month">
+<!--      <input type="range" min="0" max="12" v-model="month"/>-->
       <p> {{ month }}개월 선택함 : {{ 원룸들[누른거].price * month }} 원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
@@ -36,6 +20,14 @@ export default {
     return {
       month : 1,
     }
+  },
+  watch : { // 데이터 감시하려면
+    month(a) { // 감시하고 싶은 데이터명으로 함수를 만들면 자동으로 감시 지정
+      if (isNaN(a)) {
+        alert('문자 입력하지마셈')
+        this.month = 1;
+      }
+    },
   },
   props : { // props 는 readOnly !!
     원룸들 : Array,
