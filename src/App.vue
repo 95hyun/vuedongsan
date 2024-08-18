75b55cd1f96f421dc0d66912523addf74b7f18d6
hyun minyoung <kingmandoo95@gmail.com>
2024-08-18 18:28:19 +0900

    feat : v-for

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="menu">
-    <a>Home</a>
-    <a>Products</a>
-    <a>About</a>
+<!--    <a v-for="작명 in 몇번 반복할지">Home</a>-->
+<!--    <a v-for="작명 in 3" :key="작명">Home</a>-->
+
+<!--    메뉴들 의 개수만큼 반복된다.-->
+<!--    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>-->
+<!--    i 는 0, 1, 2 처럼 변하는 고유값-->
+<!--    <a v-for="(menus, i) in 메뉴들" :key="i">{{ menus }}</a>-->
+    <a v-for="menus in 메뉴들" :key="menus">{{ menus }}</a>
   </div>
 
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p> {{ price[0] }}</p>
+  <div v-for="(rooms, i) in 3" :key="i">
+    <h4> {{ products[i] }}</h4>
+    <a> {{ price[i] }}</a>
   </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p> {{ price[1] }}</p>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p> {{ price[2] }}</p>
-  </div>
+
+<!--  <div>-->
+<!--    <h4>{{ products[0] }}</h4>-->
+<!--    <p> {{ price[0] }}</p>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <h4>{{ products[1] }}</h4>-->
+<!--    <p> {{ price[1] }}</p>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <h4>{{ products[2] }}</h4>-->
+<!--    <p> {{ price[2] }}</p>-->
+<!--  </div>-->
 
 </template>
 
@@ -27,8 +36,13 @@ export default {
   name: 'App',
   data() {
     return {
+      rooms : {
+        products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+        price: [70, 60, 50],
+      },
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      price: [70, 60, 50]
+      price: [70, 60, 50],
+      메뉴들 : ['Home', 'Shop', 'About'],
     }
   },
   components: {
@@ -43,7 +57,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
