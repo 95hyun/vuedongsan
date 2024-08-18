ef76d6fbd3c876792936f5cbc22f20339d9b4087
hyun minyoung <kingmandoo95@gmail.com>
2024-08-18 20:23:09 +0900

    feat : 모달창에 v-for 의 id 별 값 표시해보기

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>{{ rooms[selectedRoomId].modalTitle }}</h4>
+      <p>{{ rooms[selectedRoomId].modalDetail }}</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+<!--  동적인 UI 만드는 법 :-->
+<!--  1. UI의 현재 상태를 데이터로 저장해둠-->
+<!--  2. 데이터에 따라 UI가 어떻게 보일지 작성-->
+
   <div class="menu">
     <a v-for="menus in 메뉴들" :key="menus">{{ menus }}</a>
   </div>
 
-  <div v-for="(rooms, i) in rooms" :key="i">
+  <div v-for="(rooms, id) in rooms" :key="id">
     <img :src="rooms.img" alt="room" class="room-img">
-    <h4> {{ rooms.product }}</h4>
+    <h4 @click="openModal(id)"> {{ rooms.product }}</h4>
     <a> {{ rooms.price }} 만원</a>
     <button @click="increase(i)">허위매물신고</button> <span> 신고수 : {{ rooms.신고수 }}</span>
   </div>
@@ -19,16 +31,40 @@ export default {
   data() {
     return {
       rooms : [
-        { product: '역삼동원룸', price: 70, 신고수: 0, img: require('./assets/room0.jpg') },
-        { product: '천호동원룸', price: 60, 신고수: 0, img: require('./assets/room1.jpg') },
-        { product: '마포구원룸', price: 50, 신고수: 0, img: require('./assets/room2.jpg') },
+        { id: 0,
+          product: '역삼동원룸',
+          price: 70,
+          신고수: 0,
+          img: require('./assets/room0.jpg'),
+          modalTitle: '역삼동원룸 제목',
+          modalDetail: '역삼동원룸 자세한 설명'},
+        { id: 1,
+          product: '천호동원룸',
+          price: 60,
+          신고수: 0,
+          img: require('./assets/room1.jpg'),
+          modalTitle: '천호동원룸 제목',
+          modalDetail: '천호동원룸 자세한 설명'},
+        { id: 2,
+          product: '마포구원룸',
+          price: 50,
+          신고수: 0,
+          img: require('./assets/room2.jpg'),
+          modalTitle: '마포구원룸 제목',
+          modalDetail: '마포구원룸 자세한 설명'},
       ],
       메뉴들 : ['Home', 'Shop', 'About'],
+      모달창열렸니 : false,
+      selectedRoomId: [],
     }
   },
   methods: {
     increase(i) {
       this.rooms[i].신고수 += 1;
+    },
+    openModal(id) {
+      this.모달창열렸니 = true;
+      this.selectedRoomId = id;
     },
   },
   components: {
@@ -62,5 +98,29 @@ export default {
 .room-img {
   width: 100%;
   margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+
 }
 </style>
