8429cf66097aeebaa6143c8c57445f9fb2d15a97
hyun minyoung <kingmandoo95@gmail.com>
2024-08-18 19:36:18 +0900

    feat : v-for div로 묶어서 처리

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,10 @@
     <a v-for="menus in 메뉴들" :key="menus">{{ menus }}</a>
   </div>
 
-  <div v-for="(rooms, i) in 3" :key="i">
-    <h4> {{ products[i] }}</h4>
-    <a> {{ price[i] }}</a>
+  <div v-for="(rooms, i) in rooms" :key="i">
+    <h4> {{ rooms.product }}</h4>
+    <a> {{ rooms.price }} 만원</a>
+    <button @click="rooms.신고수++">허위매물신고</button> <span> 신고수 : {{ rooms.신고수 }}</span>
   </div>
 
 <!--  <div>-->
@@ -36,17 +37,17 @@ export default {
   name: 'App',
   data() {
     return {
-      rooms : {
-        products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
-        price: [70, 60, 50],
-      },
-      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      price: [70, 60, 50],
+      rooms : [
+        { product: '역삼동원룸', price: 70, 신고수: 0},
+        { product: '천호동원룸', price: 60, 신고수: 0},
+        { product: '마포구원룸', price: 50, 신고수: 0},
+      ],
       메뉴들 : ['Home', 'Shop', 'About'],
     }
   },
   components: {
-  }
+
+  },
 }
 </script>
 
