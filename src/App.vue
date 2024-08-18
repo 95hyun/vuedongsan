6e044b4ea9a8945c9d4a91c3136d74959e0c07a4
hyun minyoung <kingmandoo95@gmail.com>
2024-08-18 19:42:43 +0900

    feat : v-for 배열에 함수 index 넣어주기

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
 <template>
   <div class="menu">
-<!--    <a v-for="작명 in 몇번 반복할지">Home</a>-->
-<!--    <a v-for="작명 in 3" :key="작명">Home</a>-->
-
-<!--    메뉴들 의 개수만큼 반복된다.-->
-<!--    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>-->
-<!--    i 는 0, 1, 2 처럼 변하는 고유값-->
-<!--    <a v-for="(menus, i) in 메뉴들" :key="i">{{ menus }}</a>-->
     <a v-for="menus in 메뉴들" :key="menus">{{ menus }}</a>
   </div>
 
   <div v-for="(rooms, i) in rooms" :key="i">
     <h4> {{ rooms.product }}</h4>
     <a> {{ rooms.price }} 만원</a>
-    <button @click="rooms.신고수++">허위매물신고</button> <span> 신고수 : {{ rooms.신고수 }}</span>
+    <button @click="increase(i)">허위매물신고</button> <span> 신고수 : {{ rooms.신고수 }}</span>
   </div>
-
-<!--  <div>-->
-<!--    <h4>{{ products[0] }}</h4>-->
-<!--    <p> {{ price[0] }}</p>-->
-<!--  </div>-->
-<!--  <div>-->
-<!--    <h4>{{ products[1] }}</h4>-->
-<!--    <p> {{ price[1] }}</p>-->
-<!--  </div>-->
-<!--  <div>-->
-<!--    <h4>{{ products[2] }}</h4>-->
-<!--    <p> {{ price[2] }}</p>-->
-<!--  </div>-->
 
 </template>
 
@@ -44,6 +24,11 @@ export default {
       ],
       메뉴들 : ['Home', 'Shop', 'About'],
     }
+  },
+  methods: {
+    increase(i) {
+      this.rooms[i].신고수 += 1;
+    },
   },
   components: {
 
